6399088a62946acd5a03c47b6e532c48129ead6a
A Glazer <avigayilglazer@gmail.com>
2019-02-17 14:44:35 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,8 @@ class Backer
   def back_project(project)
     @backed_projects << project
     # add = Project.new(backed_projects)
-    project.add_backer(self)
+     project.add_backer(self)
+
 
   end
 
