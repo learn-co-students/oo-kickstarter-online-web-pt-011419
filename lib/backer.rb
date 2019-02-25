dfe190bf1dad9d8d495a9a5ec7703d4b52090a09
Kelsey Low <kklowlow@gmail.com>
2019-02-25 02:01:46 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,6 +8,9 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
+    # adds project instance to project array of backer class
     project.add_backer(self) unless project.backers.include?(self)
+    # adds backer instance to backer array of project class
+    # if backer instance isn't already there
   end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,10 @@ class Project
   end
   
   def add_backer(backer)
-    @backers << backer
+    @backers << backer 
+    # adds backer instance to backer array of project class
     backer.back_project(self) unless backer.backed_projects.include?(self)
+    # adds project instance to project array of backer class
+    # if project instance isn't already there
   end
 end
